7f130bf519c84ed0b996faae975461369ca1a547
Sandeep Gaonkar <SandeepGa@emids.com>
2020-01-13 10:47:16 +0530

    Updated Code

--- CoreApi/Controllers/WeatherReportController.cs
+++ CoreApi/Controllers/WeatherReportController.cs
@@ -13,7 +13,7 @@ namespace CoreApi.Controllers
     {
         private static readonly string[] Summaries = new[]
        {
-            "Freezing1", "Bracing1"
+            "Freezing1", "Bracing1","Cool"
             //"Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
         // GET: api/WeatherReport
